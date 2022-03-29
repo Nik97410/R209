d95eea9f26a0abeb9d5ffa59e1de7db012d73a76
Nik97410 <90002518+Nik97410@users.noreply.github.com>
2022-03-29 11:07:42 +0400

    Create EXO5.js

---
+++ EXO5.js
@@ -0,0 +1,25 @@
+document.write("<center>");
+/* Mettre un titre pour la table */
+
+
+/* Créer la table*/
+document.write("<table border=1>");
+
+/* Créer la première ligne */
+document.write("<tr><td>","x","</td>");
+for(var i=1;i<=9;i++)
+document.write("<td>",i,"</td>");
+/* créer les autres lignes*/
+for(var i=1;i<10;i++)
+{
+/* Créer la ligne numéro i*/
+document.write("<tr>");
+document.write("<td>",i,"</td>");
+for(var j=1;j<=9;j++)
+{
+/* Créer la colonne numéro j*/
+document.write("<td>",i*j,"</td>");
+}
+document.write("</tr>");
+}
+document.write("</table>");
