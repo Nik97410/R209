1d864c5017d33b89cb55ca9fbd9e466c5949b446
Nik97410 <90002518+Nik97410@users.noreply.github.com>
2022-04-12 10:06:38 +0400

    R209

---
+++ TD3/code.php
@@ -0,0 +1,11 @@
+<?php
+$variable = "Une variable chaîne "; echo $variable;
+echo "<br />";
+// Une autre variable :
+$variable = 1000;
+echo $variable;
+echo "<br />";
+// Une autre Variable!
+$Variable = 1200;
+echo $Variable;
+?>
