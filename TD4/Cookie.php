90dfd13bd4d113ccf54cd38e8cb410a5d761d6df
Nik97410 <90002518+Nik97410@users.noreply.github.com>
2022-04-19 08:34:11 +0400

    Create Cookie.php

---
+++ TD4/Cookie.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title> test cookie</title>
+</head>
+
+<body>
+    <h1> Cookies niam niam </h1>
+    <p>
+        <?php techo $_COOKIE['toto'];?>
+    </p>
+</body>
+
+</html>
